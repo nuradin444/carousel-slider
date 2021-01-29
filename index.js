e651aaca7215fc8406a77f265ff1d23c6e6f38b1
Nuradin Sheikh <nuradin900@outlook.com>
2021-01-29 13:30:00 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,326 @@
+//Array of objects
+var productDataWomens = [{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-grey-faux-fur-longline-coat/p/619712403', imageSrc: 'https://media2.newlookassets.com/i/newlook/619712403.jpg', productTitle: 'Dark Grey Faux Fur Longline Coat', price: '45.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/tops/black-ribbed-long-sleeve-roll-neck-top/p/635105501', imageSrc: 'https://media2.newlookassets.com/i/newlook/635105501.jpg', productTitle: 'Black Ribbed Long Sleeve Roll Neck Top', price: '8.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/leggings/black-coated-leather-look-leggings/p/634018201', imageSrc: 'https://media3.newlookassets.com/i/newlook/634018201.jpg', productTitle: 'Black Coated Leather-Look Leggings', price: '19.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-faux-fur-longline-coat/p/619712401', imageSrc: 'https://media3.newlookassets.com/i/newlook/619712401.jpg', productTitle: 'Black Faux Fur Longline Coat', price: '45.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-high-waist-super-skinny-hallie-jeans/p/635654901', imageSrc: 'https://media2.newlookassets.com/i/newlook/635654901.jpg', productTitle: 'Black High Waist Super Skinny Hallie Jeans', price: '25.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/camel-revere-collar-coat/p/619653214', imageSrc: 'https://media2.newlookassets.com/i/newlook/619653214.jpg', productTitle: 'Camel Revere Collar Coat', price: '35.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-jeggings/p/623140201', imageSrc: 'https://media3.newlookassets.com/i/newlook/623140201.jpg', productTitle: 'Black Lift & Shape Jeggings', price: '17.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-aviator-jacket/p/632207701', imageSrc: 'https://media2.newlookassets.com/i/newlook/632207701.jpg', productTitle: 'Black Leather-Look Aviator Jacket', price: '55.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/light-grey-fluffy-houndstooth-double-breasted-coat-/p/631197208', imageSrc: 'https://media3.newlookassets.com/i/newlook/631197208.jpg', productTitle: 'Light Grey Fluffy Houndstooth Double Breasted Coat', price: '49.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/leggings/black-high-waist-leggings/p/616323901', imageSrc: 'https://media3.newlookassets.com/i/newlook/616323901.jpg', productTitle: 'Black High Waist Leggings', price: '8.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-coated-super-skinny-hallie-jeans/p/631910801', imageSrc: 'https://media2.newlookassets.com/i/newlook/631910801.jpg', productTitle: 'Black Coated Super Skinny Hallie Jeans', price: '27.99'},{ productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/black-leather-look-chunky-lace-up-boots/p/629029201', imageSrc: 'https://media3.newlookassets.com/i/newlook/629029201.jpg', productTitle: 'Black Leather-Look Chunky Lace Up Boots', price: '27.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-high-waist-skinny-jeans/p/631911801', imageSrc: 'https://media3.newlookassets.com/i/newlook/631911801.jpg', productTitle: 'Black Lift & Shape High Waist Skinny Jeans', price: '25.99'},{ productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/wide-fit-black-teddy-lined-chunky-lace-up-boots/p/637716701', imageSrc: 'https://media3.newlookassets.com/i/newlook/637716701.jpg', productTitle: 'Wide Fit Black Teddy Lined Chunky Lace Up Boots', price: '27.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-green-faux-fur-longline-coat/p/619712438', imageSrc: 'https://media3.newlookassets.com/i/newlook/619712438.jpg', productTitle: 'Dark Green Faux Fur Longline Coat', price: '45.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/nightwear/pale-grey-fluffy-hooded-dressing-gown/p/629877202', imageSrc: 'https://media3.newlookassets.com/i/newlook/629877202.jpg', productTitle: 'Pale Grey Fluffy Hooded Dressing Gown', price: '25.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/pale-grey-revere-collar-coat/p/619653202', imageSrc: 'https://media2.newlookassets.com/i/newlook/619653202.jpg', productTitle: 'Pale Grey Revere Collar Coat', price: '35.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-super-soft-super-skinny-india-jeans/p/623185401', imageSrc: 'https://media2.newlookassets.com/i/newlook/623185401.jpg', productTitle: 'Black Super Soft Super Skinny India Jeans', price: '22.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-biker-jacket-/p/620187701', imageSrc: 'https://media2.newlookassets.com/i/newlook/620187701.jpg', productTitle: 'Black Leather-Look Biker Jacket', price: '32.99'},{ productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/black-leather-flat-chelsea-boots/p/633518801', imageSrc: 'https://media2.newlookassets.com/i/newlook/633518801.jpg', productTitle: 'Black Leather Flat Chelsea Boots', price: '35.99'},];
+
+//selecting the elements to add popup link to
+document.getElementById('img1').addEventListener('click', function(){
+  document.getElementById('bg-modal').style.display = "inline";
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+document.getElementById('bg-modal').style.display= 'none';
+});
+
+document.getElementById('img2').addEventListener('click', function(){
+  document.getElementById('bg-modal').style.display = "inline";
+});
+
+
+
+
+
+//code to search for the carousel and return the value entered by the user
+function filter(){
+  var FilterValue, input, div, i, items;
+
+  input = document.getElementById("myInput");
+  FilterValue = input.value.toUpperCase();
+  items = document.getElementById("items");
+  div = document.getElementsByClassName('info');
+
+  for (i = 0; i< div.length; i++){
+    var a = div[i].getElementsByTagName('div')[0];
+    if(a.innerHTML.toLocaleUpperCase().indexOf(FilterValue)> -1){
+      div[i].style.display = "";
+
+    } else{
+      div[i].style.display = "none";
+    }
+
+    
+
+  }
+
+
+}
+
+
+//code to make the carousel move from left to right
+
+var span = document.getElementsByTagName('span');
+var div = document.getElementsByTagName('div');
+
+
+var l = 0;
+span[1].onclick = ()=>{
+  l++;
+  for(var i of div)
+  {
+      if(l== 0){i.style.left = "0px";}
+      if(l== 1){i.style.left = "-740px";}
+      if(l== 2){i.style.left = "-1480px";}
+      if(l== 3){i.style.left = "-2220px";}
+      if(l== 4){i.style.left = "-2550px";}
+      
+     
+
+      if(1>4){l=4;}
+
+  }  
+}
+span[0].onclick = ()=>{
+    l--;
+    for(var i of div)
+    {
+        if(l== 0){i.style.left = "0px";}
+        if(l== 1){i.style.left = "-740px";}
+        if(l== 2){i.style.left = "-1480px";}
+        if(l== 3){i.style.left = "-2220px";}
+        if(1<0){l=0;}
+  
+    }  
+  }
+
+  //below i grab the image from the array of objects and display it on the carousel. I create an element that holds the image in place
+
+ //create image1
+  var img = document.createElement("img");
+  img.src = productDataWomens[0].imageSrc
+  document.getElementById("img1").appendChild(img)
+
+   // Create image2
+   var img2 = document.createElement("img");
+   img2.src = productDataWomens[1].imageSrc
+   document.getElementById("img2").appendChild(img2)
+
+    //create image 3
+    var img3 = document.createElement("img");
+    img3.src = productDataWomens[2].imageSrc
+    document.getElementById("img3").appendChild(img3)
+
+    //create image 4
+    var img4 = document.createElement("img");
+    img4.src = productDataWomens[3].imageSrc
+    document.getElementById("img4").appendChild(img4)
+
+    //Create image 5
+    var img5 = document.createElement("img");
+    img5.src = productDataWomens[4].imageSrc
+    document.getElementById("img5").appendChild(img5)
+
+    //Create image 6
+    var img6 = document.createElement("img");
+    img6.src = productDataWomens[5].imageSrc
+    document.getElementById("img6").appendChild(img6)
+
+    //Create image 7
+    var img7 = document.createElement("img");
+    img7.src = productDataWomens[6].imageSrc
+    document.getElementById("img7").appendChild(img7)
+
+    //Create image 8
+    var img8 = document.createElement("img");
+    img8.src = productDataWomens[7].imageSrc
+    document.getElementById("img8").appendChild(img8)
+
+    //Create image 9
+    var img9 = document.createElement("img");
+    img9.src = productDataWomens[8].imageSrc
+    document.getElementById("img9").appendChild(img9)
+
+    //Create image 10
+    var img10 = document.createElement("img");
+    img10.src = productDataWomens[9].imageSrc
+    document.getElementById("img10").appendChild(img10)
+
+    //Create image 11
+    var img11 = document.createElement("img");
+    img11.src = productDataWomens[10].imageSrc
+    document.getElementById("img11").appendChild(img11)
+
+    //Create image 12
+    var img12 = document.createElement("img");
+    img12.src = productDataWomens[11].imageSrc
+    document.getElementById("img12").appendChild(img12)
+
+    //Create image 13
+    var img13 = document.createElement("img");
+    img13.src = productDataWomens[12].imageSrc
+    document.getElementById("img13").appendChild(img13)
+
+    //Create image 14
+    var img14 = document.createElement("img");
+    img14.src = productDataWomens[13].imageSrc
+    document.getElementById("img14").appendChild(img14)
+
+    //Create image 15
+    var img15 = document.createElement("img");
+    img15.src = productDataWomens[14].imageSrc
+    document.getElementById("img15").appendChild(img15)
+
+    //Create image 16
+    var img16 = document.createElement("img");
+    img16.src = productDataWomens[15].imageSrc
+    document.getElementById("img16").appendChild(img16)
+
+    //Create image 17
+    var img17 = document.createElement("img");
+    img17.src = productDataWomens[16].imageSrc
+    document.getElementById("img17").appendChild(img17)    
+
+    //Create image 18
+    var img18 = document.createElement("img");
+    img18.src = productDataWomens[17].imageSrc
+    document.getElementById("img18").appendChild(img18) 
+
+    //Create image 19
+    var img19 = document.createElement("img");
+    img19.src = productDataWomens[18].imageSrc
+    document.getElementById("img19").appendChild(img19) 
+
+    //Create image 20
+    var img20 = document.createElement("img");
+    img20.src = productDataWomens[19].imageSrc
+    document.getElementById("img20").appendChild(img20) 
+
+
+  var product_Link = document.getElementById("modal-contents");
+
+  product_Link.innerHTML +=productDataWomens[0].productUrl;
+
+ 
+
+
+
+    //Add Title name and price to the webpage
+
+    var product_info = document.getElementById("img1");
+   
+
+    product_info.innerHTML +=productDataWomens[0].productTitle;
+    product_info.innerHTML +=productDataWomens[0].price;
+
+    var product_info = document.getElementById("img2");
+
+
+    product_info.innerHTML += productDataWomens[1].productTitle;
+    product_info.innerHTML += productDataWomens[1].price;
+
+    var product_info = document.getElementById("img3");
+   
+
+    product_info.innerHTML += productDataWomens[2].productTitle;
+    product_info.innerHTML += productDataWomens[2].price;
+
+
+    var product_info = document.getElementById("img4");
+   
+
+    product_info.innerHTML += productDataWomens[3].productTitle;
+    product_info.innerHTML += productDataWomens[3].price;
+
+    var product_info = document.getElementById("img5");
+   
+
+    product_info.innerHTML += productDataWomens[4].productTitle;
+    product_info.innerHTML += productDataWomens[4].price;
+
+    var product_info = document.getElementById("img6");
+    
+
+    product_info.innerHTML += productDataWomens[5].productTitle;
+    product_info.innerHTML += productDataWomens[5].price;
+
+    var product_info = document.getElementById("img7");
+   
+
+    product_info.innerHTML += productDataWomens[6].productTitle;
+    product_info.innerHTML += productDataWomens[6].price;
+
+    var product_info = document.getElementById("img8");
+   
+
+    product_info.innerHTML += productDataWomens[7].productTitle;
+    product_info.innerHTML += productDataWomens[7].price;
+
+    var product_info = document.getElementById("img9");
+
+
+    product_info.innerHTML += productDataWomens[8].productTitle;
+    product_info.innerHTML += productDataWomens[8].price;
+
+    var product_info = document.getElementById("img10");
+    
+
+    product_info.innerHTML += productDataWomens[9].productTitle;
+    product_info.innerHTML += productDataWomens[9].price;
+
+    var product_info = document.getElementById("img11");
+    
+
+    product_info.innerHTML += productDataWomens[10].productTitle;
+    product_info.innerHTML += productDataWomens[10].price;
+
+    var product_info = document.getElementById("img12");
+    
+
+    product_info.innerHTML += productDataWomens[11].productTitle;
+    product_info.innerHTML += productDataWomens[11].price;
+
+
+    var product_info = document.getElementById("img13");
+    
+
+    product_info.innerHTML += productDataWomens[12].productTitle;
+    product_info.innerHTML += productDataWomens[12].price;
+
+    var product_info = document.getElementById("img14");
+   
+
+    product_info.innerHTML += productDataWomens[13].productTitle;
+    product_info.innerHTML += productDataWomens[13].price;
+
+    var product_info = document.getElementById("img15");
+   
+
+    product_info.innerHTML += productDataWomens[14].productTitle;
+    product_info.innerHTML += productDataWomens[14].price;
+
+    var product_info = document.getElementById("img16");
+   
+
+    product_info.innerHTML += productDataWomens[15].productTitle;
+    product_info.innerHTML += productDataWomens[15].price;
+
+    var product_info = document.getElementById("img17");
+    
+
+    product_info.innerHTML += productDataWomens[16].productTitle;
+    product_info.innerHTML += productDataWomens[16].price;
+
+    var product_info = document.getElementById("img18");
+    
+
+    product_info.innerHTML += productDataWomens[17].productTitle;
+    product_info.innerHTML += productDataWomens[17].price;
+
+    var product_info = document.getElementById("img19");
+    
+
+    product_info.innerHTML += productDataWomens[18].productTitle;
+    product_info.innerHTML += productDataWomens[18].price;
+
+    var product_info = document.getElementById("img20");
+    
+
+    product_info.innerHTML += productDataWomens[19].productTitle;
+    product_info.innerHTML += productDataWomens[19].price;
+
+
+ 
+
+
+
+
+
+
+
